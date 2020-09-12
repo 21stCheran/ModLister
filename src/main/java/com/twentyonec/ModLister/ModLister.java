0ceebe21525e671c1b23a26193dbcbf47dd8f07f
21stCheran <43900990+21stCheran@users.noreply.github.com>
2020-09-12 15:09:01 +0530

    First commit

---
+++ src/main/java/com/twentyonec/ModLister/ModLister.java
@@ -0,0 +1,11 @@
+package com.twentyonec.ModLister;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ModLister extends Plugin {
+
+	 @Override
+	    public void onEnable() {
+	        getLogger().info("Yay! It loads!");
+	    }
+}
