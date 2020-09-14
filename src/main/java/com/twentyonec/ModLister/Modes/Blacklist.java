b66befb00430b6dd26b7a84aa9442900061ed79d
21stCheran <43900990+21stCheran@users.noreply.github.com>
2020-09-14 20:26:11 +0530

    Fixed Broken Pipe error

--- src/main/java/com/twentyonec/ModLister/Modes/Blacklist.java
+++ src/main/java/com/twentyonec/ModLister/Modes/Blacklist.java
@@ -2,21 +2,31 @@ package com.twentyonec.ModLister.Modes;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import com.twentyonec.ModLister.ModLister;
 
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class Blacklist implements Mode {
 
+	ModLister plugin = ModLister.getPlugin();
+	
 	@Override
 	public void playerJoins(Map<String, String> playerMods, List<String>configMods,
 							ProxiedPlayer player) {
 		
-		configMods.forEach((key) -> {
-			if (playerMods.containsKey(key)) {
-				player.disconnect(new TextComponent("You contain a blacklisted mod called"
-													+ key));
-			}
-		});
+		plugin.getProxy().getScheduler().schedule(plugin, new Runnable() {
+			@Override
+            public void run() {
+            	configMods.forEach((key) -> {
+					if (playerMods.containsKey(key)) {
+						player.disconnect(new TextComponent("You contain a blacklisted mod called "
+															+ key));
+					}
+				});
+            }
+         }, 1, TimeUnit.SECONDS);
 	}
 }
--- src/main/java/com/twentyonec/ModLister/Modes/Whitelist.java
+++ src/main/java/com/twentyonec/ModLister/Modes/Whitelist.java
@@ -2,23 +2,34 @@ package com.twentyonec.ModLister.Modes;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import com.twentyonec.ModLister.ModLister;
 
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class Whitelist implements Mode {
+	
+	ModLister plugin = ModLister.getPlugin();
 
 	@Override
 	public void playerJoins(Map<String, String> playerMods, List<String>configMods,
 							ProxiedPlayer player) {
 		
-		playerMods.forEach((key, value) -> {
-			if (!configMods.contains(key)) {
-				player.disconnect(new TextComponent("You have an unwhitelisted mod called "
-													+ key
-													+ "version: "
-													+ value));
+		plugin.getProxy().getScheduler().schedule(plugin, new Runnable() {
+			@Override
+			public void run() {
+				playerMods.forEach((key, value) -> {
+					if (!configMods.contains(key)) {
+						player.disconnect(new TextComponent("You have an unwhitelisted mod called "
+															+ key
+															+ " "
+															+ value));
+					}
+				});
 			}
-		});
+
+         }, 1, TimeUnit.SECONDS);
 	}
 }
